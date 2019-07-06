08fcfd7f9fc977ea86eec467985d73af9f2b9960
iamabhay <abhayaman669@gmail.com>
2019-07-06 10:29:49 +0530

    Updated prime no. program

--- 12_prime_check.c
+++ 12_prime_check.c
@@ -3,19 +3,17 @@
 void main() {
 	int num=4, i, prime;
 	
+	printf("Enter a number : ");
+	scanf();
 	for(i=1;i<=num;i++){
-		if(i==1 || i==num){
-			continue;
-		}
 		if(num%i==0){
-			printf("It is not a prime no.");
-			break;
-		}else{
-			prime=1;
+			prime++;
 		}
 	}
 	
-	if(prime==1){
+	if(prime==2){
 		printf("It is a prime no.");
+	}else{
+		printf("It is not a prime no.");
 	}
 }
--- test.c
+++ test.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 
 void main() {
-	printf("Hello Welcome to my project");
+	int num=424, temp, res=0, div;
+	temp=num;
+	
+	while(num!=0){
+		div=num%10;
+		res=res*10+div;
+		num=num/10;
+	}
+	
+	printf("%d", res==temp);
 }
